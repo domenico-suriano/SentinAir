6c78996634e6c5e3baeed4c7fcd05f4a87185361
Domenico Suriano <domenico.suriano@enea.it>
2024-01-02 13:23:35 +0100

    Update sentinair_system_manager.py

--- sentinair/sentinair_system_manager.py
+++ sentinair/sentinair_system_manager.py
@@ -1,4 +1,4 @@
-# Copyright 2020   Dr. Domenico Suriano (domenico.suriano@enea.it)
+# Copyright 2023   Dr. Domenico Suriano (domenico.suriano@enea.it)
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
